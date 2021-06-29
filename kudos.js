fec6b443395c96fdfe2f2d6cc3643d782fa588df
Guilherme de Lyra <guilyra12@gmail.com>
2021-06-29 19:54:02 -0300

    finishing solution, for now

--- kudos.js
+++ kudos.js
@@ -1,32 +1,62 @@
+const { numToText } = require('./spelledOutNumber');
+
 // Conversão de kudos para pontos
 const KUDOS_TO_POINTS = [
-  { name: 'OK', value: 5 },
-  { name: 'NICE', value: 10 },
-  { name: 'GOOD', value: 20 },
-  { name: 'GREAT', value: 50 },
   { name: 'SUPER', value: 100 },
+  { name: 'GREAT', value: 50 },
+  { name: 'GOOD', value: 20 },
+  { name: 'NICE', value: 10 },
+  { name: 'OK', value: 5 },
 ];
 
 // Conversão de kudos para reais
-const KUDOS_TO_REAL = [
-  { name: 'OK', value: 2 },
-  { name: 'NICE', value: 5 },
-  { name: 'GOOD', value: 8 },
-  { name: 'GREAT', value: 15 },
-  { name: 'SUPER', value: 25 },
-];
+const KUDOS_TO_REAL = {
+  'OK': 2,
+  'NICE': 5,
+  'GOOD': 8,
+  'GREAT': 15,
+  'SUPER': 25,
+};
 
 /* 
   Recebe: um int representando o número de pontos do usuário
   Retorna: um array contendo os kudos. Ex.: ['OK', 'GOOD'] 
 */
-function getKudosForUser(points) {}
+function getKudosForUser(points) {
+  let result = [];
+  KUDOS_TO_POINTS.forEach((obj) => {
+    while (obj.value <= points) {
+      points -= obj.value;
+      result.push(obj.name);
+    }
+  });
+  return result;
+}
+
 
 /* 
   Recebe: Recebe um array contendo os nomes dos kudos de um usuário. Ex.: ['OK', 'GOOD']
   Retorna: a mensagem padrão com o valor em reais dos kudos por extenso. Ex.: Parabéns, você ganhou vinte e cinco reais
 */
-function getKudosValueMessageForUser(kudos) {}
+function valueWithCurrency(msg) {
+  if (msg === 'um milhão') return `${msg} de reais`;
+  if (msg === 'um') return `${msg} real`; // não tem como já que o valor minimo é 2 reais, mas por prevenção :)
+  return `${msg} reais`;
+} 
+
+function getKudosValueMessageForUser(kudos) {
+  const value = valueFromKudos(kudos);
+  const spelledOutValue = valueWithCurrency(numToText(value));
+  const kudosArrayMsg = kudos.join(', ');
+  return `Você recebeu ${spelledOutValue} em retorno aos kudos ${kudosArrayMsg}!`
+}
+
+
+function valueFromKudos(kudos) {
+  return kudos.reduce((acc, k) => 
+    acc + KUDOS_TO_REAL[k], 0
+  );
+}
 
 module.exports = {
   getKudosForUser,
