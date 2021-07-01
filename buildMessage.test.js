26dd87758806d016e4f9bb0d13bb6621c75f9cab
Guilherme de Lyra <guilyra12@gmail.com>
2021-07-01 16:51:28 -0300

    adding more unit tests

--- buildMessage.test.js
+++ buildMessage.test.js
@@ -1,20 +1,37 @@
 const { getKudosValueMessageForUser } = require('./buildMessage');
 
-test('test getKudosValueMessageForUser', () => {
-    expect(getKudosValueMessageForUser(['GOOD', 'NICE']))
-      .toEqual('Você recebeu treze reais em retorno aos kudos GOOD, NICE!');
-
-    expect(getKudosValueMessageForUser(['GOOD', 'GOOD']))
-      .toEqual('Você recebeu dezesseis reais em retorno aos kudos GOOD, GOOD!');
-
-    expect(getKudosValueMessageForUser(['SUPER']))
-      .toEqual('Você recebeu vinte e cinco reais em retorno aos kudos SUPER!');
+describe('test getKudosValueMessageForUser', () => {
+    it('valid ones', () => {
+        expect(getKudosValueMessageForUser(['GOOD', 'NICE']))
+        .toEqual('Você recebeu treze reais em retorno aos kudos GOOD, NICE!');
+  
+      expect(getKudosValueMessageForUser(['GOOD', 'GOOD']))
+        .toEqual('Você recebeu dezesseis reais em retorno aos kudos GOOD, GOOD!');
+  
+      expect(getKudosValueMessageForUser(['SUPER']))
+        .toEqual('Você recebeu vinte e cinco reais em retorno aos kudos SUPER!');
+  
+      expect(getKudosValueMessageForUser(['SUPER', 'GOOD', 'NICE', 'OK']))
+        .toEqual('Você recebeu quarenta reais em retorno aos kudos SUPER, GOOD, NICE, OK!');
       
-    expect(getKudosValueMessageForUser(['SUPER', 'GOOD', 'NICE', 'OK']))
-      .toEqual('Você recebeu quarenta reais em retorno aos kudos SUPER, GOOD, NICE, OK!');
-    
-    const kudosArray = Array(40000).fill('SUPER')
-    const kudosArrayMsg = kudosArray.join(', ');
-    expect(getKudosValueMessageForUser(kudosArray))
-      .toEqual(`Você recebeu um milhão de reais em retorno aos kudos ${kudosArrayMsg}!`); 
+      const kudosArray = Array(40000).fill('SUPER')
+      const kudosArrayMsg = kudosArray.join(', ');
+      expect(getKudosValueMessageForUser(kudosArray))
+        .toEqual(`Você recebeu um milhão de reais em retorno aos kudos ${kudosArrayMsg}!`); 
+    });
+  
+    it('invalid ones', () => {
+        expect(getKudosValueMessageForUser(['GOOD', 'NICE']))
+        .not.toEqual('Você recebeu quinze reais em retorno aos kudos GOOD, NICE!');
+  
+      expect(getKudosValueMessageForUser(['GOOD', 'GOOD']))
+        .not.toEqual('Você recebeu dezessete reais em retorno aos kudos GOOD, GOOD!');
+  
+      expect(getKudosValueMessageForUser(['SUPER']))
+        .not.toEqual('Você recebeu cinquenta reais em retorno aos kudos SUPER!');
+  
+      expect(getKudosValueMessageForUser(['SUPER', 'GOOD', 'NICE', 'OK']))
+        .not.toEqual('Você recebeu setenta e dois reais em retorno aos kudos SUPER, GOOD, NICE, OK!'); 
+    });
 });
+  
--- kudos.test.js
+++ kudos.test.js
@@ -1,13 +1,28 @@
 const { getKudosForUser } = require('./kudos');
 
-test('test getKudosForUser', () => {
-    expect(getKudosForUser(30)).toEqual(['GOOD', 'NICE']);
+describe('test getKudosForUser', () => {
+  it('valid ones', () => {
+      expect(getKudosForUser(30)).toEqual(['GOOD', 'NICE']);
+  
+      expect(getKudosForUser(40)).toEqual(['GOOD', 'GOOD']);
+      
+      expect(getKudosForUser(100)).toEqual(['SUPER']);
+      
+      expect(getKudosForUser(135)).toEqual(['SUPER', 'GOOD', 'NICE', 'OK']);
+      
+      expect(getKudosForUser(1000000)).toEqual(Array(10000).fill('SUPER'));
+  });
 
-    expect(getKudosForUser(40)).toEqual(['GOOD', 'GOOD']);
+  it('invalid ones', () => {
+    expect(getKudosForUser(30)).not.toEqual(['NICE', 'NICE', 'NICE']);
+
+    expect(getKudosForUser(40)).not.toEqual(['GOOD', 'NICE', 'NICE']);
     
-    expect(getKudosForUser(100)).toEqual(['SUPER']);
+    expect(getKudosForUser(100)).not.toEqual(['GREAT', 'GREAT']);
     
-    expect(getKudosForUser(135)).toEqual(['SUPER', 'GOOD', 'NICE', 'OK']);
+    expect(getKudosForUser(135)).not.toEqual(['SUPER', 'NICE', 'NICE', 'NICE', 'OK']);
     
-    expect(getKudosForUser(1000000)).toEqual(Array(10000).fill('SUPER'));
+    expect(getKudosForUser(1000000)).not.toEqual(Array(20000).fill('GREAT'));
+  });
 });
+
